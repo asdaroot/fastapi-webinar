8d9322d78560d8c80fc95de5f12091a8aff564b4
asdaroot <aastarovoytov@gmail.com>
2023-03-12 10:50:11 +0300

    Update

---
+++ venv/bin/create_db.py
@@ -0,0 +1,6 @@
+#!/home/asda/fastapi-webinar/venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'app-example==0.0.1','create_db.py'
+__requires__ = 'app-example==0.0.1'
+__import__('pkg_resources').require('app-example==0.0.1')
+__file__ = '/home/asda/fastapi-webinar/scripts/create_db.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
